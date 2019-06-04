62d6c6dda420c02a6e4c786a135064364268f413
Work User <puhkiy@gmai.com>
2019-06-04 17:24:49 +0300

    code changes

--- Class Ranges project/script.js
+++ Class Ranges project/script.js
@@ -3,23 +3,30 @@
 
 
 class Ranges {   /**    * Adds a range to the list    */
+  
   add(range) { 
-    return myArr = this.range; 
+     let myVar = range;
+     myVar.push(10);
+     this.range = myVar;
+     //console.log(myVar);
         // TODO: implement this   
   }
 
   /**    * Removes a range from the list    */
-  remove(range) {     // TODO: implement this   
-  }
+  //remove(range) {     // TODO: implement this   
+  //}
   /**    * Prints out the list of ranges    */
   print() {  
-    console.log(myArr);   // TODO: implement this   
+   console.log(this.range);
+       // TODO: implement this   
   }
 }
 // Example 
 const f = new Ranges(); 
 f.add([1, 4]); f.print(); // Should display: [1, 4] 
-r.add([10, 20]); r.print(); // Should display: [1, 4] [10, 20] 
+
+f.add([10, 20]); f.print(); // Should display: [1, 4] [10, 20] 
+/*
 r.add([10, 10]); r.print(); // Should display: [1, 4] [10, 20] 
 r.add([21, 21]);r.print(); // Should display: [1, 4] [10, 21] 
 r.add([2, 4]); r.print(); // Should display: [1, 4] [10, 21] 
@@ -29,3 +36,4 @@ r.remove([10, 10]); r.print(); // Should display: [1, 8] [11, 21]
 r.remove([10, 11]); r.print(); // Should display: [1, 8] [12, 21] 
 r.remove([15, 17]); r.print(); // Should display: [1, 8] [12, 14] [18, 21] 
 r.remove([3, 19]); r.print(); // Should display: [1, 2] [20, 21]
+*/
